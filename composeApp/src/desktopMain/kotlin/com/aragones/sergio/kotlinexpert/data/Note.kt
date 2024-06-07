67b9a3294cfd6e51dcdde9d6af6b1492f967d798
Sergio Aragonés <sergioat7@gmail.com>
2024-06-07 18:40:37 +0200

    Create notes with extension property

--- composeApp/src/desktopMain/kotlin/com/aragones/sergio/kotlinexpert/data/Note.kt
+++ composeApp/src/desktopMain/kotlin/com/aragones/sergio/kotlinexpert/data/Note.kt
@@ -1,5 +1,23 @@
 package com.aragones.sergio.kotlinexpert.data
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+
 data class Note(val title: String, val description: String, val type: Type) {
     enum class Type { TEXT, AUDIO }
+    companion object
 }
+
+val Note.Companion.fakeNotes
+    get() = flow {
+
+        delay(2000)
+        val notes = (1..10).map {
+            Note(
+                "Title $it",
+                "Description $it",
+                if (it % 3 == 0) Note.Type.AUDIO else Note.Type.TEXT
+            )
+        }
+        emit(notes)
+    }
--- composeApp/src/desktopMain/kotlin/com/aragones/sergio/kotlinexpert/ui/screens/home/HomeState.kt
+++ composeApp/src/desktopMain/kotlin/com/aragones/sergio/kotlinexpert/ui/screens/home/HomeState.kt
@@ -1,13 +1,11 @@
 package com.aragones.sergio.kotlinexpert.ui.screens.home
 
 import com.aragones.sergio.kotlinexpert.data.Note
+import com.aragones.sergio.kotlinexpert.data.fakeNotes
 import com.aragones.sergio.kotlinexpert.update
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 
 object HomeState {
@@ -15,23 +13,10 @@ object HomeState {
     private val _state = MutableStateFlow(UiState())
     val state: StateFlow<UiState> = _state
 
-    private fun getNotes(): Flow<List<Note>> = flow {
-
-        delay(2000)
-        val notes = (1..10).map {
-            Note(
-                "Title $it",
-                "Description $it",
-                if (it % 3 == 0) Note.Type.AUDIO else Note.Type.TEXT
-            )
-        }
-        emit(notes)
-    }
-
     suspend fun loadNotes() = withContext(Dispatchers.IO) {
 
         _state.update { it.copy(loading = true) }
-        getNotes().collect { notes ->
+        Note.fakeNotes.collect { notes ->
             _state.update { it.copy(notes = notes) }
         }
     }
