b3bfe11ccc921035b15498742b66c34e134126fc
Sergio Aragonés <sergioat7@gmail.com>
2024-07-08 23:07:07 +0200

    Move code to common and desktop modules

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -11,7 +10,6 @@ kotlin {
     jvm("desktop")
 
     sourceSets {
-        val desktopMain by getting
 
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -20,28 +18,17 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-        }
-        desktopMain.dependencies {
-            implementation(compose.desktop.currentOs)
             implementation(compose.materialIconsExtended)
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.3")
             implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.okhttp)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
         }
-    }
-}
 
-
-compose.desktop {
-    application {
-        mainClass = "MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.aragones.sergio"
-            packageVersion = "1.0.0"
+        val desktopMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.3")
+                implementation(libs.ktor.client.okhttp)
+            }
         }
     }
 }
---
+++ desktop/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    jvm("desktop")
+
+    sourceSets {
+
+        val desktopMain by getting {
+            dependencies {
+                implementation(project(":common"))
+                implementation(compose.desktop.currentOs)
+            }
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.aragones.sergio"
+            packageVersion = "1.0.0"
+        }
+    }
+}
