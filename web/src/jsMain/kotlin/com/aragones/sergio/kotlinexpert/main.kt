6fdcc2a3e03b52642b65d1ff76340af8b1bb2827
Sergio Aragonés <sergioat7@gmail.com>
2024-07-09 13:30:11 +0200

    Extract styles to a StyleSheet

--- web/src/jsMain/kotlin/com/aragones/sergio/kotlinexpert/main.kt
+++ web/src/jsMain/kotlin/com/aragones/sergio/kotlinexpert/main.kt
@@ -5,13 +5,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import com.aragones.sergio.kotlinexpert.data.Note
 import com.aragones.sergio.kotlinexpert.ui.screens.home.HomeViewModel
-import org.jetbrains.compose.web.css.*
+import com.aragones.sergio.kotlinexpert.ui.theme.AppStyleSheet
+import org.jetbrains.compose.web.css.Style
+import org.jetbrains.compose.web.css.marginLeft
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
 
     renderComposable(rootElementId = "root") {
+        Style(AppStyleSheet)
         val scope = rememberCoroutineScope()
         val homeViewModel = remember { HomeViewModel(scope) }
 
@@ -23,18 +27,7 @@ fun main() {
 
 @Composable
 fun NotesList(notes: List<Note>, onNoteClick: (Note) -> Unit) {
-    Div(
-        attrs = {
-            style {
-                display(DisplayStyle.Flex)
-                flexDirection(FlexDirection.Column)
-                alignItems(AlignItems.Center)
-                width(100.percent)
-                height(100.percent)
-            }
-        },
-
-        ) {
+    Div(attrs = { classes(AppStyleSheet.notesList) }) {
         notes.forEach { note ->
             NoteCard(note, onNoteClick)
         }
@@ -43,41 +36,12 @@ fun NotesList(notes: List<Note>, onNoteClick: (Note) -> Unit) {
 
 @Composable
 fun NoteCard(note: Note, onNoteClick: (Note) -> Unit) {
-    Div(
-        attrs = {
-            onClick { onNoteClick(note) }
-
-            style {
-                display(DisplayStyle.Flex)
-                flexDirection(FlexDirection.Column)
-                width(80.percent)
-                maxWidth(600.px)
-                marginTop(8.px)
-                marginBottom(8.px)
-                border(1.px, LineStyle.Solid, Color.black)
-                borderRadius(4.px)
-                padding(16.px)
-                cursor("pointer")
-            }
-        }
-    ) {
-        Div(
-            attrs = {
-                style {
-                    display(DisplayStyle.Flex)
-                    flexDirection(FlexDirection.Row)
-                    alignItems(AlignItems.Center)
-                    width(100.percent)
-                }
-            }
-        ) {
-            H3(
-                attrs = {
-                    style {
-                        flex(1)
-                    }
-                }
-            ) {
+    Div(attrs = {
+        onClick { onNoteClick(note) }
+        classes(AppStyleSheet.noteCard)
+    }) {
+        Div(attrs = { classes(AppStyleSheet.noteCardHeader) }) {
+            H3(attrs = { classes(AppStyleSheet.noteCardTitle) }) {
                 Text(note.title)
             }
 
