b5491a972f26cc51adb21edfb36bed596cb9ec57
Sergio Aragonés <sergioat7@gmail.com>
2024-07-09 14:41:17 +0200

    Change web title with KotlinJs

--- web/src/jsMain/kotlin/com/aragones/sergio/kotlinexpert/main.kt
+++ web/src/jsMain/kotlin/com/aragones/sergio/kotlinexpert/main.kt
@@ -2,13 +2,32 @@ package com.aragones.sergio.kotlinexpert
 
 import com.aragones.sergio.kotlinexpert.ui.App
 import com.aragones.sergio.kotlinexpert.ui.theme.AppStyleSheet
+import kotlinx.browser.document
+import kotlinx.browser.window
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
 
+    composeSample()
+    kotlinJsSample()
+}
+
+private fun composeSample() {
+
+    document.getElementById("root") ?: return
     renderComposable(rootElementId = "root") {
         Style(AppStyleSheet)
         App()
+    }
+}
+
+private fun kotlinJsSample() {
+
+    window.onload = {
+        val title = document.getElementById("title")
+        if (title != null) {
+            title.textContent = getAppTitle()
+        }
     }
 }
